ae17316e772f9178caf5665a93a6874832385bdb
Seunghoon-Oh <steveoh5419@gmail.com>
2023-09-08 11:01:26 +0000

    redis passwd 변경

--- data/redis.go
+++ data/redis.go
@@ -13,7 +13,7 @@ var rdb *redis.Client
 func SetupRedisClient() {
 	rdb = redis.NewClient(&redis.Options{
 		Addr:     "redis-master.cloud-ml-pipeline.svc.cluster.local:6379",
-		Password: "X1VErUTn97", // no password set
+		Password: "zWpuJLoN6d", // no password set
 		DB:       0,            // use default DB
 	})
 	n, err := rdb.Exists(ctx, "id").Result()
